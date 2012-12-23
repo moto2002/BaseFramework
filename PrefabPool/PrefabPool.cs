d83671a2194ef6f3df60c7be16539f13d5ec0f3c
Joseph Le Grice <joe.legrice@gmail.com>
2012-12-23 17:29:06 +0000

    Prefab pool updates

--- PrefabPool/PrefabPool.cs
+++ PrefabPool/PrefabPool.cs
@@ -9,17 +9,29 @@ namespace BaseFramework
 	{
 		public string m_name; //TODO : Specify Prefab Pool Name
 		public GameObject m_objectPrefab;
-		public int m_preLoadAmount = 5;
+		
+		private int m_cacheLimit = 0;
 		
 		private PrefabPoolManager m_manager;
 		private Queue<GameObject> m_pooledPrefabs;
 		private List<GameObject> m_activePrefabs;
 		
+		public int Cached
+		{
+			get { return m_cacheLimit; }
+			set { Recache (value); }
+		}
+		
+		bool Initialised
+		{
+			get { return m_manager == null || m_pooledPrefabs == null || m_activePrefabs == null || m_cacheLimit == 0; }
+		}
+		
 		void Awake ()
 		{
 		}
 		
-		void Start ()
+		void Initialise ()
 		{
 			m_manager = PrefabPoolManager.Instance;
 			m_manager.RegisterPool (m_objectPrefab.name, this);
@@ -27,14 +39,16 @@ namespace BaseFramework
 			m_pooledPrefabs = new Queue<GameObject> ();
 			m_activePrefabs = new List<GameObject> ();
 			
-			for (int i=0; i<m_preLoadAmount; i++)
-			{
-				PoolObject ();
-			}
+			Recache (4);
 		}
 		
 		void PoolObject ()
 		{
+			if (!Initialised)
+			{
+				Initialise ();
+			}
+			
 			GameObject go = Instantiate (m_objectPrefab) as GameObject;
 			go.SetActive (false);
 			go.name = go.name+"."+m_pooledPrefabs.Count;
@@ -42,9 +56,35 @@ namespace BaseFramework
 			m_pooledPrefabs.Enqueue (go);
 		}
 		
+		void Recache (int newSize) // TODO : Recache should be able to lower the number of prefabs in memory too!!
+		{
+			// How many objects to we need to Instantiate / Destroy
+			int diff = newSize - m_cacheLimit;
+			
+			// Do the Instantions / Destroys
+			if (diff < 0)
+			{
+				Debug.LogWarning ("PrefabPools cannot recache to less objects");
+				return;
+			}
+			
+			for (int i = 0; i<diff; i++)
+			{
+				PoolObject ();
+			}
+			
+			// Set m_cacheLimit to newSize
+			m_cacheLimit = newSize;
+		}
+		
 		public GameObject GetNextActive ()
 		{
-			// TODO : Pool new object if no object could be found in queue
+			if (!Initialised)
+			{
+				Initialise ();
+			}
+			
+			// TODO : Pool new object if no object could be found in queue (increase pool size)
 			if (m_pooledPrefabs.Count == 0)
 			{
 				Debug.LogError ("[Prefab Pool] No more GameObjects pooled!!");
@@ -59,10 +99,16 @@ namespace BaseFramework
 		
 		public void ReturnToPool (GameObject go)
 		{
+			if (!Initialised)
+			{
+				Initialise ();
+			}
+			
 			m_activePrefabs.Remove (go);
 			m_pooledPrefabs.Enqueue (go);
 			
 			go.SetActive (false);
 		}
+		
 	}
 }
