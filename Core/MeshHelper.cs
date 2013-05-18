ea10a7d06d2877f5343ffc13f7185a36d2390ad6
Joseph Le Grice <joe.legrice@gmail.com>
2013-05-18 17:47:58 +0100

    Implemented Basic form of MeshHelper. It's not very good.

--- Core/MeshHelper.cs
+++ Core/MeshHelper.cs
@@ -5,17 +5,35 @@ namespace BaseFramework.Core
 {
 	public static class MeshHelper
 	{
-		public static void AppendMesh( ref Mesh thisMesh, Mesh toAppend ) // todo : make extensdion method
+		public static Mesh JoinMeshes( Mesh first, Mesh second ) // todo : make extensdion method
 		{
-//			int newVertLength = thisMesh.vertices.Length + toAppend.vertices.Length;
-//			int newUVLength = thisMesh.uv.Length + toAppend.uv.Length;
-//			int newTriLength = thisMesh.triangles.Length + toAppend.triangles.Length;
-//			
-//			Vector3[] newVerts = new Vector3[ newVertLength ];
-//			Vector2[] newUVs = new Vector2[ newUVLength ];
-//			int[] newTris = new int[ newTriLength ];
-//			
-//			// TODO : How are the two sections stitched together?!
+			int newVertLength = first.vertices.Length + second.vertices.Length;
+			int newTriLength  = first.triangles.Length + second.triangles.Length;
+			
+			Vector3[] newVerts = new Vector3[ newVertLength ];
+			Vector2[] newUVs = new Vector2[ newVertLength ];
+			int[] newTris = new int[ newTriLength ];
+			
+			for ( int v=0; v<newVertLength; v++ )
+			{
+				if ( v == second.vertices.Length ) break;
+				newVerts[ v ] = v >= first.vertices.Length ? second.vertices[ v ] : first.vertices[ v ];
+				newUVs[ v ] = v >= first.vertices.Length ? second.uv[ v ] : first.uv[ v ];
+			}
+			
+			for ( int t=0; t<newTriLength; t++ )
+			{
+				if ( t == second.triangles.Length ) break;
+				newTris[ t ] = t >= first.triangles.Length ? second.triangles[ t ] : first.triangles[ t ];
+			}
+			
+			Mesh newMesh = new Mesh();
+			newMesh.vertices = newVerts;
+			newMesh.uv = newUVs;
+			newMesh.triangles = newTris;
+			newMesh.RecalculateNormals();
+			newMesh.RecalculateBounds();
+			return newMesh;
 		}
 	}
 }
