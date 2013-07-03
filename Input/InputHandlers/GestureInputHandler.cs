1999b40059e8f5e37ccd0dcea23113d5d4f9f844
Joseph Le Grice <joe.legrice@gmail.com>
2013-07-03 08:08:50 +0100

    Implemented Notifier pattern in GestureInputHandler (TODO: Implement in Gestures instead..?)

--- Input/InputHandlers/GestureInputHandler.cs
+++ Input/InputHandlers/GestureInputHandler.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using BaseFramework.Core;
 using System.Collections.Generic;
 
 
 namespace BaseFramework.InputManager
 {
-	public class GestureInputHandler : InputHandler
+	public class GestureInputHandler : InputHandler, INotifier<Vector2>
 	{
+#region InputHandler Implementation
 		protected override void InitialiseInputHandler ()
 		{
 			registeredGestures = new List<Gesture>();
 			registeredGestures.Add( new SwipeGesture() );
 			
 			m_touches = new List<InputData>();
+			m_subscriptions = new List<IObserver<Vector2>>();
 		}
 		
 		protected override InputMethod ValidInputMethods()
@@ -22,34 +25,65 @@ namespace BaseFramework.InputManager
 		public override void OnInputStart( InputData data )
 		{
 			m_touches.Add( data );
+			NotifySubscribers( );
 		}
 		
 		public override void OnInputTick( InputData data )
 		{
-			for ( int gIndex = 0; gIndex < registeredGestures.Count; gIndex++ )
-			{
-				Gesture g = registeredGestures[ gIndex ];
-				if ( g.NumberOfTouchesRequiredForGesture() == m_touches.Count )
-				{
-					g.UpdateGesture( m_touches.ToArray() ); //TODO: Something with this result
-				}
-			}
+			NotifySubscribers( );
 		}
 		
 		public override void OnInputEnd( InputData data )
 		{
+			NotifySubscribers( );
+			m_touches.Remove( data );
+		}
+#endregion
+		
+#region INotifier Implementation
+		public bool RegisterSubscription( IObserver<Vector2> subscriber )
+		{
+			if ( m_subscriptions.Contains( subscriber ) )
+			{
+				return false;
+			}
+			m_subscriptions.Add( subscriber );
+			return true;
+		}
+		
+		public bool UnregisterSubscription( IObserver<Vector2> subscriber )
+		{
+			if ( m_subscriptions.Contains( subscriber ) )
+			{
+				m_subscriptions.Remove( subscriber );
+				return true;
+			}
+			return false;
+		}
+		
+		public void NotifySubscribers ()
+		{
+			InputData[] data = m_touches.ToArray();
 			for ( int gIndex = 0; gIndex < registeredGestures.Count; gIndex++ )
 			{
 				Gesture g = registeredGestures[ gIndex ];
-				if ( g.NumberOfTouchesRequiredForGesture() == m_touches.Count )
+				if ( g.NumberOfTouchesRequiredForGesture() == data.Length )
 				{
-					g.UpdateGesture( m_touches.ToArray() ); //TODO: Something with this result
+					//TODO: Functionality such that subscribers choose which Gestures to listen to.
+					//		Could be implemented by making the Gesture implement INotifier instead.
+					Vector2 gestureResult = g.UpdateGesture( data ); //TODO: Notify Observers of result
+					for ( int sIndex = 0; sIndex < m_subscriptions.Count; sIndex++ )
+					{
+						IObserver<Vector2> sub = m_subscriptions[ sIndex ];
+						sub.OnNotify( gestureResult );
+					}
 				}
 			}
-			m_touches.Remove( data );
 		}
+#endregion
 		
-		private List<Gesture> registeredGestures;
 		private List<InputData> m_touches;
+		private List<Gesture> registeredGestures;
+		private List<IObserver<Vector2>> m_subscriptions;
 	}
 }
