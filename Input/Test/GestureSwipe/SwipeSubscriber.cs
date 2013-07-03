3de94ea94bf6bc81cb2035820a3d2c17aec7c31b
Joseph Le Grice <joe.legrice@gmail.com>
2013-07-03 08:08:07 +0100

    Implemented SwipeGesture test components.

--- Input/Test/GestureSwipe/SwipeSubscriber.cs
+++ Input/Test/GestureSwipe/SwipeSubscriber.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
-using System.Collections;
+using BaseFramework.Core;
+using BaseFramework.InputManager;
 
-namespace BaseFramework.InputManager
+
+public class SwipeSubscriber : MonoBehaviour, IObserver<Vector2>
 {
-	public class SwipeSubscriber : MonoBehaviour
+	public void OnNotify( Vector2 data )
 	{
-		
+		rigidbody.AddForce( data );
 	}
+	
+	private void Start()
+	{
+		gestureHandler = (GestureInputHandler)GestureInputHandler.Instance;
+		gestureHandler.RegisterSubscription( this );
+	}
+	
+	private GestureInputHandler gestureHandler;
 }
