93073cde75d35ae525509fb4de0accf5108d437a
Joseph Le Grice <joe.legrice@gmail.com>
2013-07-05 07:52:05 +0100

    Test Scene improved and functional!

--- Input/Test/GestureSwipe/SwipeSubscriber.cs
+++ Input/Test/GestureSwipe/SwipeSubscriber.cs
@@ -5,9 +5,17 @@ using BaseFramework.InputManager;
 
 public class SwipeSubscriber : MonoBehaviour, IObserver<Vector2>
 {
+	public float MAX_SPEED;
+	
 	public void OnNotify( Vector2 data )
 	{
-		rigidbody.AddForce( data );
+		Vector3 force = Vector3.zero;
+		force.x = data.x;
+		force.z = data.y;
+		
+		// Calculate the impulse
+		m_velocity += (force * Time.deltaTime) / rigidbody.mass;
+		m_velocity = Vector3.ClampMagnitude( m_velocity, MAX_SPEED );
 	}
 	
 	private void Start()
@@ -16,5 +24,19 @@ public class SwipeSubscriber : MonoBehaviour, IObserver<Vector2>
 		gestureHandler.RegisterSubscription( this );
 	}
 	
+	private void FixedUpdate()
+	{
+		transform.position += m_velocity * Time.deltaTime;
+	}
+	
+	private void OnCollisionEnter( Collision collisionInfo )
+	{
+		Vector3 normal = collisionInfo.contacts[0].normal;
+		Vector3 newVelocity = m_velocity - ( 2 * Vector3.Dot( m_velocity, normal ) * normal );
+		
+		m_velocity = newVelocity;
+	}
+	
+	private Vector3 m_velocity;
 	private GestureInputHandler gestureHandler;
 }
