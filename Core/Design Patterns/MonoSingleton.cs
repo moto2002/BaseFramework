4a19d96e2235bd79b49493e22e39f26d4f21a2fa
Joseph Le Grice <joe.legrice@gmail.com>
2013-12-19 21:51:59 +0000

    MonoSingleton auto-formatiing

--- Core/Design Patterns/MonoSingleton.cs
+++ Core/Design Patterns/MonoSingleton.cs
@@ -3,47 +3,45 @@ using System.Collections;
 
 namespace BaseFramework.Core
 {
-	/// <summary>
-	/// Singleton MonoBehaviour Object.
-	/// 
-	/// TODO : Locking when instantiating..?
-	/// </summary>
-	public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
-	{
-		private static T m_instance;
-		private static System.Object m_lock = new System.Object();
+    /// <summary>
+    /// Singleton MonoBehaviour Object.
+    /// 
+    /// TODO : Locking when instantiating..?
+    /// </summary>
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T m_instance;
+        private static System.Object m_lock = new System.Object();
 
-		public static T Instance
-		{
-			get
-			{
-				if (m_instance == null)
-				{
-					lock (m_lock)
-					{
-						m_instance = (FindObjectOfType (typeof (T)) as T);
+        public static T Instance {
+            get {
+                if( m_instance == null )
+                {
+                    lock( m_lock )
+                    {
+                        m_instance = (FindObjectOfType( typeof( T ) ) as T);
 						
-						if (m_instance == null)
-						{
-							GameObject go = new GameObject();
-							go.name = "Singleton."+typeof(T).ToString();
-							m_instance = go.AddComponent<T>();
-						}
+                        if( m_instance == null )
+                        {
+                            GameObject go = new GameObject();
+                            go.name = "Singleton." + typeof( T ).ToString();
+                            m_instance = go.AddComponent<T>();
+                        }
 						
-						if (m_instance == null)
-						{
-							Debug.LogError ("Singleton Instance did not instantiate properly.");
-						}
-					}
-				}
+                        if( m_instance == null )
+                        {
+                            Debug.LogError( "Singleton Instance did not instantiate properly." );
+                        }
+                    }
+                }
 				
-				return m_instance;
-			}
-		}
+                return m_instance;
+            }
+        }
 		
-		protected virtual void OnEnable()
-		{
-			name = "Singleton."+typeof(T).ToString();
-		}
-	}
+        protected virtual void OnEnable()
+        {
+            name = "Singleton." + typeof( T ).ToString();
+        }
+    }
 }
